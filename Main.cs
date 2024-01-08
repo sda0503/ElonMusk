a01556bc4f4cfa348b5fb52b0b6a7173bfd63333
sda0503 <sda0503@naver.com>
2024-01-08 10:36:26 +0900

    [Add] 더미 추가

---
+++ Main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ElonMusk
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hellow World");
+        }
+    }   
+}
+
