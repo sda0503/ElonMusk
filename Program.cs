6db867b0812d3746793a5bd59cdb91ccc91d9476
bohun <slawkd2002@gmail.com>
2024-01-10 19:39:24 +0900

    [Modify] 프로필 문구 수정
    상태창 > 이력서

--- Program.cs
+++ Program.cs
@@ -251,24 +251,19 @@ namespace Elonmusk
             Console.WriteLine();
             Console.WriteLine("[아이템 목록]");
             Console.WriteLine();
-            /*
-            for (int i = 0; i < Game.game.inventory.items.Count; i++)
             for (int i = 0; i < Game.game.player.items.Count; i++)
             {
                 (Item, bool) item = Game.game.player.items[i];
                 String strEquipped = (item.Item2) ? "[E]" : String.Empty;
                 Console.WriteLine($"- {i + 1} {strEquipped}{item.Item1.name} | {item.Item1.GetEffectScript()} | {item.Item1.desc} | {item.Item1.GOLD}");
             }
-            */
             Console.WriteLine();
             Console.WriteLine("0. 나가기");
         }
         public override void GetAction(int act)
         {
-            /*
             if (act == 0)
             {
-                Game.game.ChangeScene(new Idle());
                 Game.game.ChangeScene(new Inventory());
             }
             else if (act > 0 && act < Game.game.player.items.Count + 1)
@@ -279,11 +274,9 @@ namespace Elonmusk
             {
                 Console.WriteLine("잘못된 입력입니다.");
             }
-            */
         }
     }
 
-    /*
     public class Shop : Scene
     {
         // bool 변수는 팔렸는지의 여부(true면 구매불가)
@@ -351,8 +344,7 @@ namespace Elonmusk
             }
         }
     }
-    */
-    /*
+
     public class Buy : Scene
     {
         public override void ShowInfo()
@@ -391,8 +383,7 @@ namespace Elonmusk
         }
 
     }
-    */
-    /*
+
     public class Item : Stat
     {
         public string desc { get; private set; }
@@ -433,7 +424,6 @@ namespace Elonmusk
             return "효과 없음";
         }
     }
-    */
 
     public class Stat
     {
@@ -442,7 +432,6 @@ namespace Elonmusk
 
         public int ATK { get; protected set; }
         public int DEF { get; protected set; }
-
 
         public int GOLD { get; protected set; }
 
@@ -491,10 +480,7 @@ namespace Elonmusk
         {
             get
             {
-                
                 Stat stat = new Stat();
-                /*
-                foreach (var i in Game.game.inventory.items)
                 foreach (var i in Game.game.player.items)
                 {
                     if (i.Item2)
@@ -502,12 +488,9 @@ namespace Elonmusk
                         stat = stat + i.Item1;
                     }
                 }
-                */
                 return stat;
-                
             }
         }
-        
 
         public Stat RealtimeStat
         {
@@ -525,12 +508,12 @@ namespace Elonmusk
             DEF = 5;
             MaxHP = 100;
             CurHP = 100;
-            GOLD = 1500;
+            GOLD = 10000;
         }
 
         public void ShowPlayerProfile()
-        {
-            Console.WriteLine("상태창");
+        {                            
+            Console.WriteLine("이력서");
             Console.WriteLine($"Lv.{level}");
             Console.WriteLine($"이름 : {playerName.Item1}");
             Console.WriteLine($"직급 : {job.ToString()}");
